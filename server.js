8f6a3be9c4cc142562eca784f8dfd0500fe7535f
williamsoftwarecode <williamsoftwarecode@gmail.com>
2019-06-19 22:29:52 +0800

    Configured package.json and server.js for Heroku deployment

--- server.js
+++ server.js
@@ -80,15 +80,15 @@ router.route('/issues/delete/:id').get((req, res) => {
             res.json('Removed successfully');
     });
 });
-app.use('/', router);
-app.listen(4000, () => console.log(`Express server running on port 4000`));
+// app.use('/', router);
+// app.listen(4000, () => console.log(`Express server running on port 4000`));
 
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/property-calculator'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/property-calculator'));
 
-// app.get('/*', function(req,res) {
-//     res.sendFile(path.join(__dirname+'/dist/property-calculator/index.html'));
-// });
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/property-calculator/index.html'));
+});
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
